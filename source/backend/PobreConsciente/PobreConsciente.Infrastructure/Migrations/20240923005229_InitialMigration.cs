f93e730a3f0b24e3852340c7dab2f3b047a93bfd
Ryan <ryaangu@gmail.com>
2024-09-22 22:03:23 -0300

    📦 build: Ajustes das dependências do projeto.

---
+++ source/backend/PobreConsciente/PobreConsciente.Infrastructure/Migrations/20240923005229_InitialMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PobreConsciente.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
